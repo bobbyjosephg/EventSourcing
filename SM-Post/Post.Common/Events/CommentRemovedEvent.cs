4972feb41506680c9c5bc0fa5b9118aa26681010
bobbyjosephgeorge@gmail.com <bobbyjosephgeorge@gmail.com>
2022-11-02 12:17:47 +0530

    committed upto command Dispatcher

---
+++ SM-Post/Post.Common/Events/CommentRemovedEvent.cs
@@ -0,0 +1,20 @@
+﻿using CQRS.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Post.Common.Events
+{
+    public class CommentRemovedEvent : BaseEvent
+    {
+        public CommentRemovedEvent(string type) : base(type)
+        {
+        }
+
+        public Guid CommentId { get; set; }
+        public string CommentText { get; set; }
+
+    }
+}
---
+++ SM-Post/Post.Common/Events/CommentUpdatedEvent.cs
@@ -0,0 +1,24 @@
+﻿
+using CQRS.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Post.Common.Events
+{
+    public class CommentUpdatedEvent : BaseEvent
+    {
+        public CommentUpdatedEvent() : base(nameof(CommentUpdatedEvent) )
+        {
+        }
+
+        public Guid CommentId { get; set; }
+        public string CommentText { get; set; }
+
+        public string Username { get; set; }
+
+        public DateTime dateTime { get; set; }
+    }
+}
